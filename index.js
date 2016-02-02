cfc29bf6428d09f256830e9f2fa75d7acd7b6e61
Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
2016-02-02 18:30:41 +0200

    Change to event type callbacks.

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
+const EventEmitter = require('events');
+const util = require('util');
+
 exports = module.exports = (function () {
 
-	function ParamChecker (opts) {
-		this.setup(opts);
+	function ParamChecker (path) {
+		EventEmitter.call(this);
+		this.path = path || 'body';
 	}
 
-	ParamChecker.prototype.setup = function (opts) {
-		opts = opts || {};
-		this.path = opts.path || 'body';
-		this.status = opts.status || 400;
-		this.error = opts.error || { msg: 'Parameters missing!', success: false };
-	}
+	util.inherits(ParamChecker, EventEmitter);
 
 	ParamChecker.prototype.check = function () {
 		var args = arguments;
@@ -20,23 +19,34 @@ exports = module.exports = (function () {
 			var obj = eval('req.' + that.path);
 
 			if (!obj) {
-				throw 'Required object ' + that.path + ' was not found from the request.';
+				console.log('Express-paramchecker object "' + that.path + '" was not found!');
 				return false;
 			}
 
 			for (var i = 0; i < args.length; i++) {
 				if (typeof obj[args[i]] == 'undefined') {
-					res.status(that.status).send(that.error);
 					found = true;
+					try {
+						that.emit('error', req, res, next);
+					} catch (err) {
+						console.log('Express-paramchecker "onerror" not defined');
+						return false;
+					}
 					break;
 				}
 			}
 
 			if (!found) {
-				next();
+				try {
+					that.emit('success', req, res, next);
+				} catch (err) {
+					console.log('Express-paramchecker "onsuccess" not defined');
+					return false;
+				}
 			}
 		}
 	}
 
 	return new ParamChecker();
+
 }).call(this);
